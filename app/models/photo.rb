bfde2f9b9e52f3202ec1726ae6c2189588601c9a
renaud rouas <renaud.rouas@gmail.com>
2018-05-22 23:15:07 +0200

    cloudinary ok

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ApplicationRecord
   belongs_to :diary
+  mount_uploader :photo, PhotoUploader
 end
--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,3 +1,4 @@
 class Travel < ApplicationRecord
   belongs_to :trip
+  mount_uploader :ticket_file, PhotoUploader
 end
