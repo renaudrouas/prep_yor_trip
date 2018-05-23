2981d043243d9901c0053d572f2e4b7ae7c59e0d
renaud rouas <renaud.rouas@gmail.com>
2018-05-23 07:08:57 +0200

    Adding attributes to User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+
+  protect_from_forgery with:  :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+
+def configure_permitted_parameters
+    added_attrs = [:photo]
+    devise_parameter_sanitizer.permit(:account_update, keys: [:photo])
+  end
+
 end
