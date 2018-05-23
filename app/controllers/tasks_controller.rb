088ad22ab2257fb14a98f38603da532300df9c10
Daniel Kollner <dankollner@gmail.com>
2018-05-23 15:06:29 +0200

    Controllers and basic views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,55 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+     if @task.update(task_params)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+     @task.destroy
+     redirect_to tasks_url, notice: 'Task was successfully destroyed.'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :done)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+    #authorize @task
+  end
+
+  def marked_as_done
+    #if current_user done with task change boolean to true
+  end
 end
+
