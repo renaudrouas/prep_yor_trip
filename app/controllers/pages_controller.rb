3d40d34a7dbd33df6e8911e68b23f73dcacbe6b6
guillaume motz <guillaume.motz@gmail.com>
2018-05-23 16:50:42 +0200

    I created a simple transparent  navbar with old html and css of easy swim, working ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+  layout "home_layout", :only => [ :home ]
+  #special layout pour la home, ne pas toucher merci.
   def home
+    @transparent = true
   end
 end
