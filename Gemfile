d0d7670d5a4ead9cd081762c19f3248c501765c2
renaud rouas <renaud.rouas@gmail.com>
2018-05-24 17:35:58 +0200

    refresh

--- Gemfile
+++ Gemfile
@@ -21,6 +21,11 @@ gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'google_places_autocomplete', '~> 0.0.3'
+
+
+
+
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,34 @@ class Trip < ApplicationRecord
   has_many :tasks, dependent: :destroy
   has_many :travels, dependent: :destroy
   belongs_to :user
+
+
+# geocoded_by :full_address
+#   after_validation :geocode, if: :full_address_changed?
+
+#   def full_address
+#     "#{address}, #{zip_code} #{city} #{ISO3166::Country[country].name}"
+#   end
+
+#   def full_address_changed?
+#     address_changed? || zip_code_changed? || city_changed? || country_changed?
+#   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
