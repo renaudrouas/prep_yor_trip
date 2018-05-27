f3c69c775de58182fba957df48fe38af127dac96
renaud rouas <renaud.rouas@gmail.com>
2018-05-27 20:28:29 +0200

    en attente pull

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,26 +2,6 @@
 import GMaps from 'gmaps/gmaps.js';
 import autocomplete from '../components/autocomplete';
 
-
-
-// const mapElement = document.getElementById('map');
-// if (mapElement) { // don't try to build a map if there's no div#map to inject in
-//   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-//   const markers = JSON.parse(mapElement.dataset.markers);
-//   map.addMarkers(markers);
-//   if (markers.length === 0) {
-//     map.setZoom(2);
-//   } else if (markers.length === 1) {
-//     map.setCenter(markers[0].lat, markers[0].lng);
-//     map.setZoom(14);
-//   } else {
-//     map.fitLatLngBounds(markers);
-//   }
-// }
-
-// autocomplete();
-
-
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
@@ -30,7 +10,7 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     let new_marker = map.addMarker(marker);
     new_marker.addListener('click', function() {
       console.log("click on marker");
-      window.location.pathname = "../" + marker.id;
+      window.location.pathname = "pathname" + marker.id;
     });
   });
 
